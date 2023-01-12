a678f68daf9cc27e0f2355e1888b380963bb44e4
Andrew Herrity McNier <andrew.mcnier@fdmgroup.com>
2023-01-12 16:46:55 +0000

    tries to make webpage work but to no avail

--- fav-teams/script.js
+++ fav-teams/script.js
@@ -1,2 +1,29 @@
-const productData = JSON.parse(rawProductData);
+const productData = JSON.parse(teamData);
 console.log(productData);
+
+const container = document.querySelector('.container');
+
+const template = (team) => {`
+    <div>
+        <h3>${team.name}</h3>
+        <h5>${team.description}</h5>
+        <div>
+            <h5>Achievements: </h5>
+            ${team.achievements.forEach = (achievement) => {
+                `${achievement} </br>`
+            }}
+        </div>
+    </div>`
+}
+
+const printOutData = (arr) => {
+    let content = ""
+    arr.forEach((team) => {
+        content += template(team);
+    });
+    container.innerHTML =+ content;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    printOutData(productData);
+  });
